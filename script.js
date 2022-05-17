3159d9c92b53cfbf76b4d9d4be65ea0db91acf05
Dmitry Korochkin <abramslost@gmail.com>
2022-05-17 16:40:19 +0300

    Выполнил ДЗ усложненное

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const num = 266219;
-const array = ("" + num).split("").map(Number);
-console.log(array);
+
+let lang = 'ru';
+
+if (lang === 'ru') {
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+} else {
+    console.log('');
+}
 
 
-let result = array.reduce(function(sum, current) {
-    
-    return sum * current;
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        break;
+    default:
+        console.log('');
+}
 
-    
-});
+
+let langArr = [];
+langArr.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+langArr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(langArr[lang]);
+
+const langArray = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+console.log(langArray[lang]);
 
 
-console.log(result**3);
+const namePerson = prompt('Как ваше имя?');
 
-console.log(String(result**3).slice(0,2));
+console.log((namePerson === 'Артем') ? 'директор' :
+    (namePerson === 'Максим') ? 'преподаватель' :
+    'студент');
+
